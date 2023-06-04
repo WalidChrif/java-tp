65d877840b8779f9f2b62dbd9723c462919e548e
Walid Chrif <103291201+WalidChrif@users.noreply.github.com>
2023-06-04 01:34:26 +0100

    fix swagger-ui

--- src/main/java/com/tppfe/controllers/StudentController.java
+++ src/main/java/com/tppfe/controllers/StudentController.java
@@ -2,6 +2,10 @@ package com.tppfe.controllers;
 
 import com.tppfe.models.StudentDTO;
 import com.tppfe.services.StudentService;
+import com.tppfe.services.StudentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,29 +15,34 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentController {
 
-    StudentService studentService;
+    private StudentService studentService;
+    public static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
 
     public StudentController(@Qualifier("studentService1") StudentService studentService) {
         this.studentService = studentService;
     }
 
     @PostMapping
-    public StudentDTO saveStudent(StudentDTO studentDTO){
+    public Long saveStudent(@RequestBody StudentDTO studentDTO){
+        LOGGER.debug("Controller.save() StudentDTO : {}" + studentDTO);
         return studentService.saveStudent(studentDTO);
     }
 
     @PutMapping
-    public StudentDTO updateStudent(StudentDTO studentDTO){
+    public Long updateStudent(@RequestBody StudentDTO studentDTO){
+        LOGGER.debug("Controller.update() StudentDTO : {}" + studentDTO);
         return studentService.updateStudent(studentDTO);
     }
 
-    @DeleteMapping("/{id}")
-    public boolean deleteStudent(@PathVariable long id){
+    @DeleteMapping("/{ids}")
+    public boolean deleteStudent(@PathVariable("ids") long id){
+        LOGGER.debug("Controller.delete() StudentDTO id : {}" + id);
         return studentService.deleteStudent(id);
     }
 
     @GetMapping
     public List<StudentDTO> selectAll(){
+        LOGGER.debug("Controller.selectAll()" );
        return  studentService.selectAll();
     }
 
--- src/main/java/com/tppfe/repositories/StudentRepository.java
+++ src/main/java/com/tppfe/repositories/StudentRepository.java
@@ -1,12 +1,15 @@
 package com.tppfe.repositories;
 
-import com.tppfe.models.StudentDTO;
+import com.tppfe.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface StudentRepository {
-    StudentDTO saveStudent(StudentDTO studentDTO);
-    StudentDTO updateStudent(StudentDTO studentDTO);
-    boolean deleteStudent(long id);
-    List<StudentDTO> selectAll ();
+
+@Repository("studentRepo1")
+public interface StudentRepository  {
+    Student save(Student student);
+    boolean deleteById(long id);
+    List<Student> findAll ();
 }
